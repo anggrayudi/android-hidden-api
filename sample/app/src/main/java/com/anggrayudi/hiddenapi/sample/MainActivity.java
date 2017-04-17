3b564f0eaaa851341ff7baccfec4e1f5faffaffc
Anggrayudi H <hardiannicko@gmail.com>
2017-04-17 22:01:03 +0700

    Nougat 7.1

--- sample/app/src/main/java/com/anggrayudi/hiddenapi/sample/MainActivity.java
+++ sample/app/src/main/java/com/anggrayudi/hiddenapi/sample/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
 import android.text.format.Formatter;
 import android.util.Log;
 import android.view.MenuItem;
@@ -38,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null)
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Editable editable;
 
         final ArrayList<Model> items = new ArrayList<>();
 
